443a2d292044899ba9509c8a6b6a867781dcb279
Eyuel Belete <eyuelbelete98@gmail.com>
2023-10-07 00:53:13 +0300

    update

---
+++ provider.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { MantineProvider, MantineThemeOverride } from '@mantine/core';
+
+import { theme as baseTheme } from './theme';
+
+import './globals.css';
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css'; 
+
+export default function RootStyleRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const theme: Partial<MantineThemeOverride> = baseTheme;
+
+  return <MantineProvider theme={theme}>{children}</MantineProvider>;
+}
